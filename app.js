dfa150cef7853584b2f4d8ef0044f0fb432b8dbb
Max Bondarenko <jaderystudion@gmail.com>
2023-05-23 18:53:54 +0300

    Add basic routes and error handling to app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const rateRouter = require("./routes/rate");
+const subscriptionRouter = require("./routes/subscription");
+
+const app = express();
+
+app.use(express.json());
+app.use("/api/rate", rateRouter);
+app.use("/api/subscription", subscriptionRouter);
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: "Page not found" });
+});
+
+app.use((err, req, res, next) => {
+  if (err.custom)
+    return res.status(err.statusCode).json({ error: err.message });
+
+  console.log(err);
+  res.status(500).json({ error: "Unknown error, try again later" });
+});
+
+module.exports = app;
