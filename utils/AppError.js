1b6d47fb0401514e9c1c8b171aabbbbf4aac060d
Max Bondarenko <jaderystudion@gmail.com>
2023-05-23 20:48:34 +0300

    "Add separate files for error handling"

---
+++ utils/AppError.js
@@ -0,0 +1,11 @@
+module.exports = class extends Error {
+  constructor(statusCode, message) {
+    super(message);
+
+    this.custom = true;
+    this.statusCode = statusCode;
+    this.status = `${statusCode}`.startsWith("4") ? "fail" : "error";
+
+    Error.captureStackTrace(this, this.constructor);
+  }
+};
