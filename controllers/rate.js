3b2b0909df5e0cb80ccb3c687c3c598091394f6d
Max Bondarenko <jaderystudion@gmail.com>
2023-05-23 20:15:46 +0300

    Add error case to rate route

--- controllers/rate.js
+++ controllers/rate.js
@@ -1,12 +1,17 @@
 const axios = require("axios");
 
-exports.convert = async (req, res, next) => {
+const AppError = require("../utils/AppError");
+const catchAsync = require("../utils/catchAsync");
+
+exports.convert = catchAsync(async (req, res, next) => {
   const response = await axios.get(
     "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=uah"
   );
 
+  if (!response.data.bitcoin.uah) return next(new AppError(400, ""));
+
   res.status(200).json({
     status: "success",
-    rate: response.data.bitcoin.uah,
+    rate: response.data.bitcoin.uah * 1,
   });
-};
+});
