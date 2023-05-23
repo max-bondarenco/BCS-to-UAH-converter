b689a11193fe8c0d1e2dc40a2d27ec343255050a
Max Bondarenko <jaderystudion@gmail.com>
2023-05-23 20:54:25 +0300

    Move file IO logic to fileIO.js

--- controllers/subscription.js
+++ controllers/subscription.js
@@ -1,8 +1,6 @@
-const fs = require("fs");
-const util = require("util");
-
 const catchAsync = require("../utils/catchAsync");
 const AppError = require("../utils/AppError");
+const fileIO = require("../utils/fileIO");
 
 exports.sendEmails = (req, res, next) => {};
 
@@ -10,21 +8,10 @@ exports.subscribe = catchAsync(async (req, res, next) => {
   const email = req.body.email;
   if (!email) return next(new AppError(400, "No email provided"));
 
-  const emails = (
-    await util.promisify(fs.readFile)(`${__dirname}/../data/emails.txt`)
-  )
-    .toString()
-    .split(",");
-
+  const emails = fileIO.readEmails();
   if (emails.includes(email))
     return next(new AppError(409, "Email already subscribed"));
 
   emails.push(email);
-
-  await util.promisify(fs.writeFile)(
-    `${__dirname}/../data/emails.txt`,
-    `${emails}`
-  );
-
   res.status(200).json({ status: "success", message: "E-mail додано" });
 });
---
+++ utils/fileIO.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+const util = require("util");
+
+exports.readEmails = async () => {
+  return (await util.promisify(fs.readFile)(`${__dirname}/../data/emails.txt`))
+    .toString()
+    .split(",");
+};
+
+exports.writeEmails = async () => {
+  await util.promisify(fs.writeFile)(
+    `${__dirname}/../data/emails.txt`,
+    `${emails}`
+  );
+};
