91051e83725277128f4ddff90f5e6f093b9dfa0f
Max Bondarenko <jaderystudion@gmail.com>
2023-05-24 20:22:40 +0300

    Add server logic to server.go

---
+++ server.go
@@ -0,0 +1,22 @@
+package converterapi
+
+import (
+	"context"
+	"net/http"
+)
+
+type Server struct {
+	httpServer *http.Server
+}
+
+func (s *Server) Run(port string) error {
+	s.httpServer = &http.Server{
+		Addr: ":"+port,		
+	}
+
+	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error { 
+	return s.httpServer.Shutdown(ctx)
+}
